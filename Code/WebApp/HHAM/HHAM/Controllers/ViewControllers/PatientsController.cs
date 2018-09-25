5a84d777d59b2c69690da99ae4f635bb25eb1b5e
RedGhoul <avaneesab5@gmail.com>
2018-09-25 15:26:47 -0400

    tested the initalization of the azure blob service

--- Code/WebApp/HHAM/HHAM/Controllers/ViewControllers/PatientsController.cs
+++ Code/WebApp/HHAM/HHAM/Controllers/ViewControllers/PatientsController.cs
@@ -9,13 +9,14 @@ using System.Web;
 using System.Web.Mvc;
 using HHAM.Models;
 using HHAM.ViewModels;
+using HHAM.Services;
 
 namespace HHAM.Controllers
 {
     public class PatientsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
+        private AzureBlobService azureBlobService = new AzureBlobService();
         // GET: Patients
         public ActionResult Index()
         {
