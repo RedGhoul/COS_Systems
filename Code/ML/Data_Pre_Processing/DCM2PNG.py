c8cc376325a5aee3e8582cc8211114758411d42b
RedGhoul <avaneesab5@gmail.com>
2018-09-26 23:40:45 -0400

    made string litreals into vars

--- Code/ML/Data_Pre_Processing/DCM2PNG.py
+++ Code/ML/Data_Pre_Processing/DCM2PNG.py
@@ -4,7 +4,7 @@ import mritopng
 # mritopng.convert_file('/home/user/DICOM/SCAN1', '/home/user/output.png', auto_contrast=True)
 
 CDM_FOLDER_LOCATION = 'A:\SPIE_AAPMLungCT\SPIE-AAPMLungCTChallenge'
-PNG_FOLDER_LOCATION = 'A:\SPIE_AAPMLungCT\'
-PNG_FOLDER_NAME = 'AAPMLungCTChallengePNG'
+PNG_FOLDER_LOCATION = 'A:\SPIE_AAPMLungCT'
+PNG_FOLDER_NAME = '\AAPMLungCTChallengePNG'
 
 mritopng.convert_folder(CDM_FOLDER_LOCATION, PNG_FOLDER_LOCATION + PNG_FOLDER_NAME, auto_contrast=True)
