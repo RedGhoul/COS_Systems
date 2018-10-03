c207cf885f75f8e6f6f37e3a2a36fa08b5b18855
RedGhoul <avaneesab5@gmail.com>
2018-10-02 22:49:04 -0400

    started to configure AutoMapper

---
+++ Code/WebApp/HHAM/HHAM/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HHAM.DataTransferObjects;
+using HHAM.Models;
+using HHAM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HHAM.App_Start
+{
+    public class AutoMapperConfig : Profile 
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Patient, PatientProfileViewModel>();
+            CreateMap<Patient, CreatePatientViewModel>();
+            CreateMap<Patient, PatientDto>();
+
+            CreateMap<Scan, ScanDto>();
+            CreateMap<Scan, CreateScanViewModel>();
+
+
+            CreateMap<CreatePatientViewModel, Patient>();
+            CreateMap<PatientDto, Patient>();
+            CreateMap<ScanDto, Scan>();
+            CreateMap<CreateScanViewModel, Scan>();
+        }
+    }
+}
