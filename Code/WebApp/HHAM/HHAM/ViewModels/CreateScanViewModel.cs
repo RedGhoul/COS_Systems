b151c888ee0cda8ee3dfcf65d5663e3ab280163e
RedGhoul <avaneesab5@gmail.com>
2018-10-03 22:07:55 -0400

    added Create View model

--- Code/WebApp/HHAM/HHAM/ViewModels/CreateScanViewModel.cs
+++ Code/WebApp/HHAM/HHAM/ViewModels/CreateScanViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultipartDataMediaFormatter.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,11 @@ namespace HHAM.ViewModels
 {
     public class CreateScanViewModel
     {
+        public int Id { get; set; }
+        public string PatientNumber { set; get; }
+        public HttpPostedFileBase file { get; set; }
+        public string Name { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Notes { get; set; }
     }
 }
