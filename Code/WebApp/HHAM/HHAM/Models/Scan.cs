80abd96407652047dc6845174c1864bfb3017175
RedGhoul <avaneesab5@gmail.com>
2018-10-09 18:01:09 -0400

    changed a few props

--- Code/WebApp/HHAM/HHAM/Models/Scan.cs
+++ Code/WebApp/HHAM/HHAM/Models/Scan.cs
@@ -10,8 +10,7 @@ namespace HHAM.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime DateAdded { get; set; }
-        public string ScanURL { get; set; }
-        public string ScanURLProcessed { get; set; }
+        public string RAW_URLProcessed { get; set; }
         public string RAW_URL { get; set; }
         public string Notes { get; set; }
     }
